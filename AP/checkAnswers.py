eb34ca2491454d8754accbd87370c62fcf61ee02
jpfefferman24 <jpfefferman@gmail.com>
2023-05-07 14:57:10 -0700

    Update checkAnswers.py

--- AP/checkAnswers.py
+++ AP/checkAnswers.py
@@ -1,41 +1,44 @@
 def main():
 
-    myAnswers = []
-    correctAnswers = []
-    correct = 0
-    incorrect = 0
-    total = 0
+	myAnswers = []
+	correctAnswers = []
+	correct = 0
+	incorrect = 0
+	total = 0
 
-    myAnswersFile = open("MyAnswers2.txt", 'r')
-    for line in myAnswersFile:
-        line = line.strip()
-        lineList = line.split("\n")
-        myAnswer = lineList[0]
-        myAnswers.append(myAnswer)
-    myAnswersFile.close()
+	answersFileName = input("File with your answers: ")
+	keyFileName = input("File with answer key: ")
 
-    correctAnswersFile = open("CorrectAnswers2.txt", 'r')
-    for line in correctAnswersFile:
-        line = line.strip()
-        lineList = line.split("\n")
-        correctAnswer = lineList[0]
-        correctAnswers.append(correctAnswer)
-    correctAnswersFile.close()
+	myAnswersFile = open(answersFileName, 'r')
+	for line in myAnswersFile:
+		line = line.strip()
+		lineList = line.split("\n")
+		myAnswer = lineList[0]
+		myAnswers.append(myAnswer)
+	myAnswersFile.close()
 
-    for i in range(len(myAnswers)):
-        if myAnswers[i] == correctAnswers[i]:
-            correct += 1
-            #print("#%i: Correct" % (i+1))
-        else:
-            incorrect += 1
-            print("#%i: Incorrect" % (i+1))
-            print("You answered: %s, Correct Answer: %s\n" % (myAnswers[i], correctAnswers[i]))
-        total += 1
+	correctAnswersFile = open(keyFileName, 'r')
+	for line in correctAnswersFile:
+		line = line.strip()
+		lineList = line.split("\n")
+		correctAnswer = lineList[0]
+		correctAnswers.append(correctAnswer)
+	correctAnswersFile.close()
 
-    print("\n-----------")
-    print("Number Correct: %i" % (correct))
-    print("Number Incorrect: %i" % (incorrect))
-    print("Percentage Correct: %i" % ((correct/total)*100))
+	for i in range(len(myAnswers)):
+		if myAnswers[i] == correctAnswers[i]:
+			correct += 1
+			#print("#%i: Correct" % (i+1))
+		else:
+			incorrect += 1
+			print("#%i: Incorrect" % (i+1))
+			print("You answered: %s, Correct Answer: %s\n" % (myAnswers[i], correctAnswers[i]))
+		total += 1
+
+	print("\n-----------")
+	print("Number Correct: %i" % (correct))
+	print("Number Incorrect: %i" % (incorrect))
+	print("Percentage Correct: %i" % ((correct/total)*100))
 
 
 
